d10fe494447ffc9d3ac0d6f116fb3fb924d0c8fc
paul.v.estipona.ctr <paul_estipona@yahoo.com>
2023-03-22 01:45:06 -0400

    Added Project to GitHub

--- build-a-formik-form_starter/src/App.js
+++ build-a-formik-form_starter/src/App.js
@@ -16,6 +16,9 @@ function App() {
         validate: values => {
             let errors = {};
             if (!values.email) errors.email = 'Field required';
+            else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
             if (!values.password) errors.password = 'Field required';
             return errors;
         }
